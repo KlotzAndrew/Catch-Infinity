f37693e91526c097c6d3b817f662cfe816f6c6c9
Andrew Klotz <andrew.klotz@hotmail.com>
2015-10-06 23:18:59 -0400

    api needs a throttle

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,11 +11,13 @@ class Stock < ActiveRecord::Base
 	private
 	def self.update_current_price(tickers)
 		yahoo_tickers = tickers.map {|x| "'" + x + "'"}.join(', ')
-		url = 'https://query.yahooapis.com/v1/public/yql?q='
-		url += URI.encode("SELECT * FROM yahoo.finance.quotes WHERE symbol IN (#{yahoo_tickers})")
-		url += '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
-		data = open(url, {:read_timeout=>3}).read
-		parse_today_data(data)
+		if yahoo_tickers.count > 0
+			url = 'https://query.yahooapis.com/v1/public/yql?q='
+			url += URI.encode("SELECT * FROM yahoo.finance.quotes WHERE symbol IN (#{yahoo_tickers})")
+			url += '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+			data = open(url, {:read_timeout=>3}).read
+			parse_today_data(data)
+		end
 	end
 
 	def self.parse_today_data(data)
