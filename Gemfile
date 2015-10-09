4a69df2998a72ed90fe2447ddc3aeb68aa5787ce
Andrew Klotz <andrew.klotz@hotmail.com>
2015-10-08 20:05:49 -0400

    need tests for current price

--- Gemfile
+++ Gemfile
@@ -23,3 +23,11 @@ group :production do
 	gem 'pg'
 	gem 'rails_12factor'
 end
+
+group :test do
+	gem 'minitest-reporters', '1.0.5'
+	gem 'guard-minitest', '2.3.1'
+	gem 'mini_backtrace',     '0.1.3'
+	gem 'webmock', '1.21.0'
+	gem 'vcr', '~> 2.9.3'
+end
--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@ZZZ_TO = stocks(:ZZZ_TO)
+  	@RSY_V = stocks(:RSY_V)
+  	@ECO_TO = stocks(:ECO_TO)
+  end
+
+  test 'correctly updates current stock data' do
+  	Stock.current_price
+  	assert_equal "Sleep Country", @ZZZ_TO.name
+  	assert_equal 99.99, @ZZZ_TO.last_price
+  	assert_equal Time.now, @ZZZ_TO.last_trade 
+  	assert_equal "Toronto Stack Exchange", @ZZZ_TO.stock_exchange
+  end
 end
