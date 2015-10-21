4d3ed6064152901b0a719618079937bde5d5390c
Andrew Klotz <andrew.klotz@hotmail.com>
2015-10-21 01:52:10 -0400

    backtest should return a value

--- lib/catch_infinity/calculator/backtests.rb
+++ lib/catch_infinity/calculator/backtests.rb
@@ -12,8 +12,8 @@ module Calculator
 		QUERY_END_BUFFER = 1.day.ago
 
 		def initialize(options = {})
-			@query_start = options[:query_start] || 1.year.ago.strftime("%Y-%m-%d")
-			@query_end = options[:query_end] || Time.now.strftime("%Y-%m-%d")
+			@query_start = options[:query_start] || 1.year.ago
+			@query_end = options[:query_end] || Time.now
 			@value_start = options[:value_start]
 			@dollar_cost_average = options[:dollar_cost_average]
 			@sell_signal = options[:sell_signal]
@@ -24,12 +24,107 @@ module Calculator
 		def calculate
 			# guard_clauses
 			check_and_fetch_missing_histories
-			# grind_numbers
-			puts "STOCKS: #{@stocks}"
-			return {}
+			grind_numbers
 		end
 
+
 		private
+		
+		def grind_numbers
+			collected_histories = collect_stock_histories
+			history_run(collected_histories)
+		end
+
+		def history_run(collected_histories)
+			avg_20_day = {}
+			avg_50_day = {}
+			stock_holdings = {}
+			total_value = @value_start
+			collected_histories.each do |day, histories|
+				histories.each do |k, v|
+					change_moving_averages(k, v, avg_20_day, avg_50_day)
+					adjust_holdings(k, v, avg_20_day, avg_50_day, stock_holdings, total_value)
+				end
+			end
+			# liquidate excess holdings
+			return total_value
+		end
+
+		def change_moving_averages(key, value, avg_20_day, avg_50_day)
+			move_20_day(key, value, avg_20_day)
+			move_50_day(key, value, avg_50_day)
+		end
+
+		def move_20_day(key, value, avg_20_day)
+			avg_20_day[key] = [] unless avg_20_day[key]
+			avg_20_day[key] << value
+			avg_20_day[key].shift if avg_20_day[key].count > 20
+		end
+
+		def move_50_day(key, value, avg_50_day)
+			avg_50_day[key] = [] unless avg_50_day[key]
+			avg_50_day[key] << value
+			avg_50_day[key].shift if avg_50_day[key].count > 50
+		end
+
+		def adjust_holdings(key, value, avg_20_day, avg_50_day, stock_holdings, total_value)
+			buy(key, value, stock_holdings, total_value) if not_holding?(key, stock_holdings) && have_money?(total_value) && should_buy?(key, value, avg_20_day, avg_50_day)
+			sell(key, value, stock_holdings, total_value) if !not_holding?(key, stock_holdings) && should_sell?(key, value, avg_20_day, avg_50_day)
+		end
+
+		def should_sell?(key, value, avg_20_day, avg_50_day)
+			avg50 = BigDecimal.new(avg_50_day[key].sum)/BigDecimal.new(avg_50_day[key].count)
+			avg20 = BigDecimal.new(avg_20_day[key].sum)/BigDecimal.new(avg_20_day[key].count)
+			return true if value > avg20 && avg20 > avg50
+		end 
+
+		def sell(key, value, stock_holdings, total_value)
+			total_value += (stock_holdings[key]*value)
+			stock_holdings[key] = 0
+		end
+
+		def not_holding?(key, stock_holdings)
+			stock_holdings[key] ||= 0
+			return true if stock_holdings[key] == 0
+			return false
+		end
+
+		def have_money?(total_value)
+			return true if total_value > 1
+		end
+
+		def should_buy?(key, value, avg_20_day, avg_50_day)
+			avg50 = BigDecimal.new(avg_50_day[key].sum)/BigDecimal.new(avg_50_day[key].count)
+			avg20 = BigDecimal.new(avg_20_day[key].sum)/BigDecimal.new(avg_20_day[key].count)
+			return true if value < avg20 && avg20 < avg50
+		end
+
+		def buy(key, value, stock_holdings, total_value)
+			invest_value = calculate_invest_value(total_value)
+			quantity = (invest_value/value).floor
+			stock_holdings[key] += quantity
+			total_value -= (quantity*value)
+		end
+
+		def calculate_invest_value(total_value)
+			if total_value < @value_start*0.1
+				total_value
+			else
+				@value_start*0.1
+			end
+		end
+
+
+		def collect_stock_histories
+			collected_histories = {}
+			@stocks.each do |stock|
+				stock.histories.each do |history|
+					collected_histories[history.date] = {} unless collected_histories[history.date]
+					collected_histories[history.date][history.stock_id] = history.price_day_close
+				end		
+			end
+			return collected_histories
+		end
 
 		def check_and_fetch_missing_histories
 			@stocks.each do |stock|
--- test/catch_infinity/calculator/backtests_test.rb
+++ test/catch_infinity/calculator/backtests_test.rb
@@ -7,22 +7,20 @@ class BacktestsTest < ActionController::TestCase
 	end
 
 	test "return a number of trades" do
-		# VCR.use_cassette("yahoo_finance") do
-		# 	options = {
-		# 		query_start: DateTime.new(2015,10,19),
-		# 		query_end: (DateTime.new(2015,10,19) - 1.year),
-		# 		value_start: 10000,
-		# 		dollar_cost_average: false,
-		# 		sell_signal: "p>20>50",
-		# 		buy_signal: "p<20<50"
-		# 	}
-		# 	calculator = Calculator::Backtests.new(options)
-		# 	answers = calculator.calculate
-		# 	assert_equal answers[:backtest],
-		# 		{
-		# 			value_end: BigDecimal.new("0.14")
-		# 		}
-		# end
+		VCR.use_cassette("yahoo_finance") do
+			options = {
+				query_start: DateTime.new(2015,10,19),
+				query_end: (DateTime.new(2015,10,19) - 1.year),
+				value_start: 10000,
+				dollar_cost_average: false,
+				sell_signal: "p>20>50",
+				buy_signal: "p<20<50",
+				stocks: [@tesla]
+			}
+			calculator = Calculator::Backtests.new(options)
+			answers = calculator.calculate
+			assert_equal answers, 14000
+		end
 	end
 
 	test "fetches historical stock data if needed" do
