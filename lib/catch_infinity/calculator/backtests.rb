8ddff5864c4cb86052852909defcb06abc3e6bd1
Andrew Klotz <andrew.klotz@hotmail.com>
2015-10-20 23:17:45 -0400

    backtest should fetch or create required histories

--- lib/catch_infinity/calculator/backtests.rb
+++ lib/catch_infinity/calculator/backtests.rb
@@ -6,7 +6,10 @@ module Calculator
 		attr_reader :dollar_cost_average
 		attr_reader :sell_signal
 		attr_reader :buy_signal
+		attr_reader :stocks
 
+		QUERY_START_BUFFER = 1.week
+		QUERY_END_BUFFER = 1.day.ago
 
 		def initialize(options = {})
 			@query_start = options[:query_start] || 1.year.ago.strftime("%Y-%m-%d")
@@ -15,10 +18,45 @@ module Calculator
 			@dollar_cost_average = options[:dollar_cost_average]
 			@sell_signal = options[:sell_signal]
 			@buy_signal = options[:buy_signal]
+			@stocks = options[:stocks]
 		end
 
 		def calculate
+			# guard_clauses
+			check_and_fetch_missing_histories
+			# grind_numbers
+			puts "STOCKS: #{@stocks}"
 			return {}
 		end
+
+		private
+
+		def check_and_fetch_missing_histories
+			@stocks.each do |stock|
+				histories = stock.histories.order(date: :asc)
+				unless far_back_enough?(histories) and recent_enough?(histories)
+					create_stock_histories(stock.ticker)
+				end
+			end
+		end
+
+		def far_back_enough?(histories)
+			return true if histories.first.date < (@query_start - QUERY_START_BUFFER)
+		end
+
+		def recent_enough?(histories)
+			return true if histories.last.date > QUERY_END_BUFFER
+		end
+
+		def create_stock_histories(ticker)
+			options = {
+				query_start: @query_start,
+				query_end: @query_end
+			}
+	    fetcher = Fetcher::Histories.new([ticker], options)
+	    history_hashes = fetcher.fetch
+	    History.insert_or_update(history_hashes)
+	  end
+
 	end
 end
--- test/catch_infinity/calculator/backtests_test.rb
+++ test/catch_infinity/calculator/backtests_test.rb
@@ -3,27 +3,45 @@ require_relative '../../../lib/catch_infinity/calculator/backtests'
 
 class BacktestsTest < ActionController::TestCase
 	def setup
+		@tesla = stocks(:tesla)
 	end
 
-	# test "return a number of trades" do
-	# 	VCR.use_cassette("yahoo_finance") do
-	# 		options = {
-	# 			query_start: DateTime.new(2015,10,19),
-	# 			query_end: (DateTime.new(2015,10,19) - 1.year),
-	# 			value_start: 10000,
-	# 			dollar_cost_average: false,
-	# 			sell_signal: "p>20>50",
-	# 			buy_signal: "p<20<50"
-	# 		}
-	# 		calculator = Calculator::Backtests.new(options)
-	# 		answers = calculator.calculate
-	# 		assert_equal answers[:backtest],
-	# 			{
-	# 				value_end: BigDecimal.new("0.14")
-	# 			}
-	# 	end
-	# end
+	test "return a number of trades" do
+		# VCR.use_cassette("yahoo_finance") do
+		# 	options = {
+		# 		query_start: DateTime.new(2015,10,19),
+		# 		query_end: (DateTime.new(2015,10,19) - 1.year),
+		# 		value_start: 10000,
+		# 		dollar_cost_average: false,
+		# 		sell_signal: "p>20>50",
+		# 		buy_signal: "p<20<50"
+		# 	}
+		# 	calculator = Calculator::Backtests.new(options)
+		# 	answers = calculator.calculate
+		# 	assert_equal answers[:backtest],
+		# 		{
+		# 			value_end: BigDecimal.new("0.14")
+		# 		}
+		# end
+	end
 
-	# test "fetches historical stock data if needed" do
-	# end
+	test "fetches historical stock data if needed" do
+		VCR.use_cassette("yahoo_finance") do
+			options = {
+				query_start: DateTime.new(2015,10,19),
+				query_end: (DateTime.new(2015,10,19) - 1.year),
+				value_start: 10000,
+				dollar_cost_average: false,
+				sell_signal: "p>20>50",
+				buy_signal: "p<20<50",
+				stocks: [@tesla]
+			}
+			assert_operator @tesla.histories.order(date: :asc).first.date, :>=, 1.year.ago
+			
+			calculator = Calculator::Backtests.new(options)
+			answers = calculator.calculate
+
+			assert_operator @tesla.histories.order(date: :asc).first.date, :<=, 1.year.ago
+		end
+	end
 end
